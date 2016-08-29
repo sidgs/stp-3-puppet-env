80ffcf78056a5cd7fe08ab04bfebdebbaf613b9c
ajay <ayeluri@gmail.com>
2016-08-29 17:43:21 -0400

    added Tomcat setup Stuff

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,105 +1,77 @@
 
-node 'wiki.hsd1.pa.comcast.net'  {
-
-  file { '/info.txt':
-    ensure => 'present',
-    content => inline_template("Created by puppet at <%= Time.now %>\n"),
+node 'tomcat-server-1'  {
+  class {'linux':}
+  # install package
+  # CATALINA_HOME
+  tomcat::install {'/opt/tomcat8' :
+    source_url => 'http://apache.osuosl.org/tomcat/tomcat-8/v8.5.4/bin/apache-tomcat-8.5.4.tar.gz',
   }
-
-  class {'linux':}
-  class {'mediawiki':}
-
-}
-node 'srikanth.hsd1.pa.comcast.net' {
-  $wikitestsitename = 'wikitest'
-  $wikitestmetanamespace = 'Wikitest'
-  $wikitestserver = "http://172.31.0.210"
-  $wikitestdbserver = 'localhost'
-  $wikitestdbname ='wikitest'
-  $wikitestdbuser ='root'
-  $wikitestdbpassword = 'training'
-  $wikitestupgradekey = 'puppet'
-
-  
-  class {'linux':}
-  class {'mediawiki':}
-}
-
-node 'afrozubuntu.hsd1.pa.comcast.net' {
-
-$wikitestsitename = 'wikitest'
-  $wikitestmetanamespace = 'Wikitest'
-  $wikitestserver = "http://172.31.0.210"
-  $wikitestdbserver = 'localhost'
-  $wikitestdbname ='wikitest'
-  $wikitestdbuser ='root'
-  $wikitestdbpassword = 'training'
-  $wikitestupgradekey = 'puppet'
-
-
-
-  class {'linux':}
-  class {'mediawiki':}
-}
-node 'shruthi.hsd1.pa.comcast.net' {
- 
-  $wikitestsitename = 'wikitest'
-  $wikitestmetanamespace = 'Wikitest'
-  $wikitestserver = "http://172.31.0.203"
-  $wikitestdbserver = 'localhost'
-  $wikitestdbname ='wikitest'
-  $wikitestdbuser ='root'
-  $wikitestdbpassword = 'training'
-  $wikitestupgradekey = 'puppet'
-
-  class {'linux':}
-  class {'mediawiki':}
-}
-node 'nagendra.hsd1.pa.comcast.net' {
-
-  class {'linux':}
-  class {'mediawiki':}
-
-}
-node 'ip-172-31-51-138.ec2.internal' {
-
-  class {'linux':}
-  class {'mediawiki':}
-
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
+  # Instance Identifier
+  tomcat::instance {'default' :
+    catalina_home => '/opt/tomcat8',
   }
+  # Shutdown Port
+  tomcat::config::server { 'default' :
+    catalina_base => '/opt/tomcat8',
+    port => '8006',
+  }
+  tomcat::config::server::connector {'default' :
+    catalina_base => '/opt/tomcat8',
+    port          => '8081',
+    protocol      => 'HTTP/1.1',
+    additional_attributes => {
+      'redirectPort' => '8443',
+    }
+  }
+  # setup service , ensure that it is running
+  tomcat::service {
+    service_enable => true,
+    service_ensure => 'running',
+  }
+  # deploy the project 'petclinic'
+}
+
+node 'jenkins' {
+  class {'linux':}
+}
+
+node 'database' {
+
+  class {'linux':}
+}
+
+node 'sonarqube' {
+
+  class {'linux':}
+}
+
+node 'webserver' {
+
+  class {'linux':}
 
 }
 
-class linux {
-
-  $admintools = ['git', 'nano', 'screen']
-
-  package { $admintools:
-    ensure => 'installed',
+# these are the set of foundational services
+class linux_all {
+  $admintools = ['git','nano','screen']
+  package { $admintools
+    ensure => 'installed'
   }
 
   $ntpservice = $osfamily ? {
     'redhat' => 'ntpd',
     'debian' => 'ntp',
-    default  => 'ntp',
+     default => 'ntp',
   }
 
-  file { '/info.txt':
-    ensure  => 'present',
-    content => inline_template("Created by Puppet at <%= Time.now %>\n"),
-  }
-
-  package { 'ntp':
+  package { $ntpservice
     ensure => 'installed',
   }
-
-  service { $ntpservice:
-    ensure => 'running',
+  service { $ntpservice
+    ensure =>  'running',
     enable => true,
   }
 
 }
+
 
